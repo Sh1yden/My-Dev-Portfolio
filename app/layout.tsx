f07d96b1259416bdb79bd9848f896f848c20f6ce
Sh1yden <pvasa8917@gmail.com>
2026-06-16 00:03:05 +0300

    delete not needed icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export const metadata: Metadata = {
   authors: [{ name: "Kirill (Shayden & Sh1yden)" }],
   icons: {
     icon: [{ url: "/icon.svg", type: "image/svg+xml" }],
-    apple: "/apple-icon.png",
   },
 };
 
